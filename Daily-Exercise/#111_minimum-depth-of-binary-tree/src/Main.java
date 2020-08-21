fb537130a9132c9391dc20aa53a28185622c633c
white <202418628@qq.com>
2020-08-21 15:34:53 +0800

    feat(Daily-Exercise): 2020/8/21, #111. minimum depth of binary tree

---
+++ Daily-Exercise/#111_minimum-depth-of-binary-tree/src/Main.java
@@ -0,0 +1,33 @@
+/**
+ * #111. 二叉树的最小深度：
+ * 给定一个二叉树，找出其最小深度。
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ * 说明: 叶子节点是指没有子节点的节点。
+ */
+
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null) return 1 + minDepth(root.right);
+        if (root.right == null) return 1 + minDepth(root.left);
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
