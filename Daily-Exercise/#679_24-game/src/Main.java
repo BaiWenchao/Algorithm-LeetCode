d207bb3892b1c0963ab0caad9fc5a43c9ab58664
white <202418628@qq.com>
2020-08-22 10:22:12 +0800

    feat(Daily-Exercise): 2020/8/22, #679 24 game

---
+++ Daily-Exercise/#679_24-game/src/Main.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * #679. 24点游戏：
+ * 你有 4 张写有 1 到 9 数字的牌。你需要判断是否能通过 *，/，+，-，(，) 的运算得到 24。
+ * 注意:
+ * 除法运算符 / 表示实数除法，而不是整数除法。例如 4 / (1 - 2/3) = 12 。
+ * 每个运算符对两个数进行运算。特别是我们不能用 - 作为一元运算符。
+ * 例如，[1, 1, 1, 1] 作为输入时，表达式 -1 - 1 - 1 - 1 是不允许的。
+ * 你不能将数字连接在一起。例如，输入为 [1, 2, 1, 2] 时，不能写成 12 + 12 。*/
+
+class Solution {
+    private static final int TARGET = 24;
+    private static final double EPSILON = 1e-6;
+    private static final int ADD = 0, MULTIPLY = 1, SUBTRACT = 2, DIVIDE = 3;
+
+    public boolean judgePoint24(int[] nums) {
+        // 为了实现浮点数组的动态增删，选用ArrayList维护浮点数据
+        List<Double> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add((double) num);
+        }
+        return solve(list);
+    }
+
+    public boolean solve(List<Double> list) {
+        if (list.size() == 0) {
+            return false;
+        }
+        if (list.size() == 1) {
+            return Math.abs(list.get(0) - TARGET) < EPSILON;
+        }
+        int size = list.size();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                // 从列表中找出两个不同的元素：
+                if (i != j) {
+                    // 新建ArrayList，将原列表中的其他元素添加到新列表中
+                    List<Double> list2 = new ArrayList<>();
+                    for (int k = 0; k < size; k++) {
+                        if (k != i && k != j) {
+                            list2.add(list.get(k));
+                        }
+                    }
+
+                    for (int k = 0; k < 4; k++) {
+                        // 加法和乘法具有交换律，所以如果之前计算过，就不用重复计算了
+                        if (k < 2 && i > j) {
+                            continue;
+                        }
+                        if (k == ADD) {
+                            list2.add(list.get(i) + list.get(j));
+                        } else if (k == MULTIPLY) {
+                            list2.add(list.get(i) * list.get(j));
+                        } else if (k == SUBTRACT) {
+                            list2.add(list.get(i) - list.get(j));
+                        } else if (k == DIVIDE) {
+                            // 除数不能是0（小于1e-6）
+                            if (Math.abs(list.get(j)) < EPSILON) {
+                                continue;
+                            } else {
+                                list2.add(list.get(i) / list.get(j));
+                            }
+                        }
+                        if (solve(list2)) {
+                            return true;
+                        }
+                        list2.remove(list2.size() - 1);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 2, 1, 2};
+        System.out.println(solution.judgePoint24(nums));
+    }
+}
