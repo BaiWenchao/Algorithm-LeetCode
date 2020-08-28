a595f720e1702e417283174bfaa741ba1d26a0af
white <202418628@qq.com>
2020-08-28 09:15:25 +0800

    feat(Daily-Exercise): 2020/8/28, #657. robot return to origin

---
+++ Daily-Exercise/#657_robot-return-to-origin/src/Main.java
@@ -0,0 +1,36 @@
+/**
+ * #657. 机器人是否能回到原点：
+ * 在二维平面上，有一个机器人从原点 (0, 0) 开始。给出它的移动顺序，判断这个机器人在完成移动后是否在 (0, 0) 处结束。
+ * 移动顺序由字符串表示。字符 move[i] 表示其第 i 次移动。机器人的有效动作有 R（右），L（左），U（上）和 D（下）。
+ * 如果机器人在完成所有动作后返回原点，则返回 true。否则，返回 false。
+ * 注意：机器人“面朝”的方向无关紧要。 “R” 将始终使机器人向右移动一次，“L” 将始终向左移动等。此外，假设每次移动机器人的移动幅度相同。*/
+
+class Solution {
+    public boolean judgeCircle(String moves) {
+        char[] movement = moves.toCharArray();
+        int up = 0;
+        int down = 0;
+        int left = 0;
+        int right = 0;
+
+        for (char c : movement) {
+            if (c == 'U') {
+                up++;
+            } else if (c == 'D') {
+                down++;
+            } else if (c == 'L') {
+                left++;
+            } else {
+                right++;
+            }
+        }
+
+        return up == down && left == right;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
