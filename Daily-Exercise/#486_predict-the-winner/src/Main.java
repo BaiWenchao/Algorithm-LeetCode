b561d0e21aae189c0f223b620b0918c82cb6d6fd
white <202418628@qq.com>
2020-09-01 23:08:15 +0800

    feat(Daily-Exercise): 2020/9/1, #486. predict the winner

---
+++ Daily-Exercise/#486_predict-the-winner/src/Main.java
@@ -0,0 +1,28 @@
+/**
+ * #486. 预测赢家
+ * 给定一个表示分数的非负整数数组。 玩家 1 从数组任意一端拿取一个分数，随后玩家 2 继续从剩余数组任意一端拿取分数，
+ * 然后玩家 1 拿，…… 。每次一个玩家只能拿取一个分数，分数被拿取之后不再可取。直到没有剩余分数可取时游戏结束。
+ * 最终获得分数总和最多的玩家获胜。
+ * 给定一个表示分数的数组，预测玩家1是否会成为赢家。你可以假设每个玩家的玩法都会使他的分数最大化。*/
+
+class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+        int length = nums.length;
+        int[][] dp = new int[length][length];
+        for (int i = 0; i < length; i++) {
+            dp[i][i] = nums[i];
+        }
+        for (int i = length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < length; j++) {
+                dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][length - 1] >= 0;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
