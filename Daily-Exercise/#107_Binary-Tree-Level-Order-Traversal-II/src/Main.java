eb3b800e2f31a05f09f188e78b8dd97a36a20adf
white <202418628@qq.com>
2020-09-06 11:02:30 +0800

    feat(Daily-Exercise): 2020/9/6, #107. binary tree level order traversal II

---
+++ Daily-Exercise/#107_Binary-Tree-Level-Order-Traversal-II/src/Main.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * #107. 二叉树的层次遍历：
+ * 给定一个二叉树，返回其节点值自底向上的层次遍历。
+ * （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ */
+
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> levelOrder = new LinkedList<>();
+        if (root == null) {
+            return levelOrder;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                TreeNode left = node.left, right = node.right;
+                if (left != null) {
+                    queue.offer(left);
+                }
+                if (right != null) {
+                    queue.offer(right);
+                }
+            }
+            levelOrder.add(0, level);
+        }
+        return levelOrder;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
