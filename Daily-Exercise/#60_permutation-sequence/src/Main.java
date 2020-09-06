124a72cc34f011e68a7eb34b332c1423a11f920c
white <202418628@qq.com>
2020-09-06 10:46:49 +0800

    feat(Daily-Exercise): 2020/9/6, #60. permutation sequence

---
+++ Daily-Exercise/#60_permutation-sequence/src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+/**
+ * #60. 第k个排列：
+ * 给出集合 [1,2,3,…,n]，其所有元素共有 n! 种排列。
+ *
+ * 按大小顺序列出所有排列情况，并一一标记，当 n = 3 时, 所有排列如下：
+ * "123"
+ * "132"
+ * "213"
+ * "231"
+ * "312"
+ * "321"
+ * 给定 n 和 k，返回第 k 个排列。
+ *
+ * 说明：
+ * 给定 n 的范围是 [1, 9]。
+ * 给定 k 的范围是[1,  n!]。*/
+
+class Solution {
+    public String getPermutation(int n, int k) {
+        int[] factorial = new int[n];
+        factorial[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            factorial[i] = factorial[i - 1] * i;
+        }
+
+        --k;
+        StringBuffer ans = new StringBuffer();
+        int[] valid = new int[n + 1];
+        Arrays.fill(valid, 1);
+        for (int i = 1; i <= n; ++i) {
+            int order = k / factorial[n - i] + 1;
+            for (int j = 1; j <= n; ++j) {
+                order -= valid[j];
+                if (order == 0) {
+                    ans.append(j);
+                    valid[j] = 0;
+                    break;
+                }
+            }
+            k %= factorial[n - i];
+        }
+        return ans.toString();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
